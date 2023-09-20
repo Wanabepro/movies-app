7a94012834114423bda60091e2e61b662ba45bb7
Sergei Iliasov <ilyasov_sk@mail.ru>
2023-09-20 15:14:48 +0300

    Responsive design added

--- src/components/film/film.js
+++ src/components/film/film.js
@@ -35,6 +35,34 @@ class Film extends Component {
 
     return (
       <article className="film">
+        <header className="film__mobile-header">
+          <img
+            className="film__mobile-poster"
+            src={
+              posterPath
+                ? `https://image.tmdb.org/t/p/original/${posterPath}`
+                : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYA8-qGTLi1IgFbY7XZHm0Fjd2qYUFBrvlZw&usqp=CAU'
+            }
+            alt="Film poster"
+          />
+          <div>
+            <div className="film__header">
+              <h2 className="film__heading">{title}</h2>
+              <div className="film__rating" style={{ borderColor: ratingColorResolver(rating) }}>
+                {rating.toFixed(1)}
+              </div>
+            </div>
+            {formatedDate && <span className="film__date">{formatedDate}</span>}
+            <ul className="film__genres">
+              {genres.map((genreId) => (
+                <li key={genreId}>
+                  <FilmGenre genreId={genreId} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </header>
+
         <img
           className="film__poster"
           src={
